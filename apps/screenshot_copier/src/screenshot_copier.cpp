56afb2922a7371538032a855d8b4c13aa320516d
H1MSK <ksda47832338@outlook.com>
2023-04-07 10:49:52 +0800

    refactor: lambda->function

--- apps/screenshot_copier/src/screenshot_copier.cpp
+++ apps/screenshot_copier/src/screenshot_copier.cpp
@@ -15,6 +15,48 @@ std::list<QString> monitored_paths;
 std::shared_ptr<QFileSystemWatcher> monitor;
 std::shared_ptr<SideAssist::Qt::Client> client;
 
+std::shared_ptr<SideAssist::Qt::NamedValue> monitored_path;
+std::shared_ptr<SideAssist::Qt::NamedValue> filename;
+std::shared_ptr<SideAssist::Qt::NamedValue> timestamp;
+
+void updateMonitoredPath(const QJsonValue& val) {
+  // TODO: use builtin validator
+  if (!val.isArray())
+    monitored_path->setValue(QJsonArray());
+  monitor->removePaths(monitor->directories());
+  qInfo("Resetting monitored paths...");
+  for (auto itr : val.toArray()) {
+    if (!itr.isString())
+      continue;
+    QString path = itr.toString();
+    QFileInfo info(path);
+    if (!info.isDir())
+      continue;
+    monitor->addPath(path);
+    qInfo("Added monitor path %s", qPrintable(path));
+  }
+}
+
+void tryCopyImage(const QString& path) {
+  qInfo("Detected directory %s changed.", qPrintable(path));
+  for (auto info : QDir(path).entryInfoList(QDir::Filter::Files, QDir::Time)) {
+    auto time = info.lastModified().toMSecsSinceEpoch();
+    if (time < timestamp->value().toInteger()) {
+      qInfo("Nothing new in dir %s.", qPrintable(path));
+      break;
+    }
+    QImage img(info.canonicalFilePath());
+    if (img.isNull())
+      continue;
+    qInfo("New image %s in dir %s.", qPrintable(info.fileName()),
+          qPrintable(path));
+    QGuiApplication::clipboard()->setImage(img);
+    filename->setValue(info.canonicalFilePath());
+    timestamp->setValue(time);
+    break;
+  }
+}
+
 int main(int argc, char* argv[]) {
   QGuiApplication app(argc, argv);
   client = std::make_shared<SideAssist::Qt::Client>();
@@ -22,13 +64,13 @@ int main(int argc, char* argv[]) {
 
   client->setClientId("screenshot_copier");
   client->installDefaultMessageHandler();
-  
+
   client->setUsername("side_assist_client");
   client->setPassword("16509490");
 
-  auto monitored_path = client->addOption("monitored_path");
-  auto filename = client->addParameter("filename");
-  auto timestamp = client->addParameter("timestamp");
+  monitored_path = client->addOption("monitored_path");
+  filename = client->addParameter("filename");
+  timestamp = client->addParameter("timestamp");
 
   client->connectToHost();
 
@@ -38,45 +80,9 @@ int main(int argc, char* argv[]) {
 
   QObject::connect(monitored_path.get(),
                    &SideAssist::Qt::NamedValue::valueChanged,
-                   [&](const QJsonValue& val) {
-                     // TODO: use builtin validator
-                     if (!val.isArray())
-                       monitored_path->setValue(QJsonArray());
-                     monitor->removePaths(monitor->directories());
-                     qInfo("Resetting monitored paths...");
-                     for (auto itr : val.toArray()) {
-                       if (!itr.isString())
-                         continue;
-                       QString path = itr.toString();
-                       QFileInfo info(path);
-                       if (!info.isDir())
-                         continue;
-                       monitor->addPath(path);
-                       qInfo("Added monitor path %s", qPrintable(path));
-                     }
-                   });
+                   updateMonitoredPath);
 
-  QObject::connect(
-      monitor.get(), &QFileSystemWatcher::directoryChanged,
-      [&](const QString& path) {
-        qInfo("Detected directory %s changed.", qPrintable(path));
-        for (auto info : QDir(path).entryInfoList(
-                 QDir::Filter::Files, QDir::Time)) {
-          auto time = info.lastModified().toMSecsSinceEpoch();
-          if (time < timestamp->value().toInteger()) {
-            qInfo("Nothing new in dir %s.", qPrintable(path));
-            break;
-          }
-          QImage img(info.canonicalFilePath());
-          if (img.isNull())
-            continue;
-          qInfo("New image %s in dir %s.", qPrintable(info.fileName()),
-                qPrintable(path));
-          app.clipboard()->setImage(img);
-          filename->setValue(info.canonicalFilePath());
-          timestamp->setValue(time);
-          break;
-        }
-      });
+  QObject::connect(monitor.get(), &QFileSystemWatcher::directoryChanged,
+                   tryCopyImage);
   return app.exec();
 }
